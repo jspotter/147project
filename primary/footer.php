525d9779f6c23475adb9463a70e471803bae835a
Jonathan Potter <potterjs61490@gmail.com>
2012-11-14 15:55:51 -0800

    Divided site into alternate and primary.

---
+++ primary/footer.php
@@ -0,0 +1,19 @@
+<!--div data-role="footer" data-id="samebar" data-position="fixed" data-tap-toggle="false">
+	<div data-role="navbar" data-grid="c">
+	<ul>
+		<li>
+			<a href="./index.php" id="currentweek">Back to This Week</a>
+		</li>
+		<li>
+			<a href="./index.php#<?= $week ?>" id="lastweek">Back to Week <?= $week ?></a>
+		</li>
+		<li>
+			<a href="./game.php" id="lastgame">Back to Game</a>
+		</li>
+		<li>
+			<a href="./search.php" id="search">Look Up a Term</a>
+		</li>
+	</ul>
+	</div>
+</div-->
+
