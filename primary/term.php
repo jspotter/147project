87515652d20483664431b41e686742eb1490d9c7
Jonathan Potter <potterjs61490@gmail.com>
2012-11-14 20:27:49 -0800

    Added optimizely script.

--- primary/term.php
+++ primary/term.php
@@ -36,6 +36,7 @@
 
 <html>
 	<head>
+		<script src="http://cdn.optimizely.com/js/138697994.js"></script>
 		<title>Football 4 Noobz</title>
 		<meta charset="utf-8">
 		<meta name="apple-mobile-web-app-capable" content="yes">
