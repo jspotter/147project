de97952e87c7aeadd7171276571cd8aacae19a59
Jonathan Potter <potterjs61490@gmail.com>
2012-11-14 16:17:16 -0800

    Finally restructured things correctly, I think.

---
+++ secondary/header.php
@@ -0,0 +1,24 @@
+<div data-role="header">
+	<?php
+		if ($backLink != "")
+		{
+	?>
+			<a href="<?= $backLink ?>">
+				Back
+			</a>
+	<?php
+		}
+		else
+		{
+	?>
+			<a href="tutorial.php">
+				Tutorial
+			</a>
+	<?php
+		}
+	?>
+	<h1>Football 4 Noobz</h1>
+	<a href="search.php?week=<?= $week ?>&gameId=<?= $gameId ?>" class="ui-btn-right">
+		Search
+	</a>
+</div><!-- /header -->
